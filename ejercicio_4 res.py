b8cee8f9cda5db1488e6b890983084ed16c1442d
germanamontiel <93627366+germanamontiel@users.noreply.github.com>
2021-11-12 16:50:39 -0300

    Ejercicios y de profundización resueltos
    El 4 ejercicio de profundización casi no lo puedo resolver pero era tan sencillo.

---
+++ ejercicio_4 res.py
@@ -0,0 +1,33 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+# Si desea puede modificar el código para ingresar más palabras
+print('Ingrese primera palabra:')
+palabra_1 = str(input())
+
+print('Ingrese segunda palabra:')
+palabra_2 = str(input())
+
+print('Ingrese tercera palabra:')
+palabra_3 = str(input())
+
+print('Ingrese cuarta palabra:')
+palabra_4 = str(input())
+
+print('Ingrese quinta palabra:')
+palabra_5 = str(input())
+
+# De cada palabra debe tomar la primera letra y armar el acrónimo
+# Ejemplo: Alumbrado, barrido y limpieza --> ABL
+# Imprimir el resultado en pantalla
+
+print (palabra_1[0] + palabra_2[0] + palabra_3[0] + palabra_4[0] + palabra_5[0])
